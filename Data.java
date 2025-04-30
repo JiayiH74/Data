8f7fd0b8da5e780363dcd52ad3fe414e470b539e
JiayiH74 <jiayih12@nycstudents.net>
2025-04-30 15:48:52 +0000

    update

--- Data.java
+++ Data.java
@@ -1,1 +1,44 @@
+public class Data
+{
+    public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
+    private int[][] grid;
 
+    
+    public Data (int cols, int rows)
+    {
+        grid = new int[cols][rows];
+    }
+    public void repopulate()
+    {
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; i++)
+            {
+                int val = 0;
+                while(val % 10 != 0 || val % 100 == 0)
+                {
+                    val = (int) Math.random() * MAX + 1;
+                }
+                grid[i][j] = val;
+            }
+        }
+    }
+
+    public String toString() {
+        String s = "";
+        for(int row[] : grid)
+        {
+            for(int col : row)
+            {
+                s += col + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    public int countIncreasingCols()
+    {
+        return 0;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-
+public class Main
+{
+    public static void main(String[] args) {
+        Data a = new Data(2, 4);
+        a.repopulate();
+        System.out.println(a);
+    }
+}
