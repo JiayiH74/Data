7d317f9f41d8550f69e3265b2bebfb67437a9727
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 14:23:44 +0000

    done

--- Data.java
+++ Data.java
@@ -3,11 +3,16 @@ public class Data
     public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
     private int[][] grid;
 
-    
     public Data (int cols, int rows)
     {
         grid = new int[cols][rows];
     }
+
+    public Data(int[][] arr)
+    {
+        grid = arr;
+    }
+
     public void repopulate()
     {
         for(int i = 0; i < grid.length; i++)
@@ -39,6 +44,16 @@ public class Data
 
     public int countIncreasingCols()
     {
-        return 0;
+        int count = 0;
+        for(int col = 0; col < grid[0].length; col++)
+        {
+            int increasing = 1;
+            for(int row = 1; row < grid.length; row++)
+            {
+                if(grid[row][col] >= grid[row - 1][col]) increasing++;
+                if(increasing == grid.length) count++;
+            }
+        }
+        return count;
     }
 }
